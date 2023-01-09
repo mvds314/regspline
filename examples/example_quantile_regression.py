5a6a3ae31026dd8d5632e9dbfdf09d79b1e8f7f1
mvds314 <postaanmartin@gmail.com>
2023-01-09 10:42:27 +0100

    Quantile regression example update

--- examples/example_quantile_regression.py
+++ examples/example_quantile_regression.py
@@ -16,21 +16,24 @@ np.random.seed(100)
 
 # Make data
 with Timer("Initializing data"):
-    x = np.linspace(0, 1, 500)
     knots = [0, 0.5, 1]
     coeffs = [0, 1, 1]
     spline = LinearSpline(knots, coeffs)
-    y = spline(x)
+    signal = lambda x:  spline(x)
+    noise = lambda x: x**2+0.1
+    x = np.linspace(0, 1, 500)
+    y = signal(x)
     xx = np.repeat(x, 10)
-    yy = spline(xx) + (xx**2 + 0.1) * np.random.randn(*xx.shape)
+    yy = signal(xx) + noise(xx) * np.random.randn(*xx.shape)
     quantiles = [0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95]
+    n_knots=5
 
 
 # Get quantiles by fitting a linear spline
 with Timer("Running linear spline quantile regression with sklearn"):
     skllsq = {}
     for q in quantiles:
-        skllsq[q] = LinearSpline(np.linspace(0, 1, 8), None)
+        skllsq[q] = LinearSpline(np.linspace(0, 1, n_knots), None)
         model = QuantileRegressor(quantile=q, alpha=0, solver="highs")
         res = model.fit(skllsq[q].eval_basis(xx), yy)
         skllsq[q].coeffs = np.append(res.intercept_, res.coef_)
@@ -40,7 +43,7 @@ with Timer("Running linear spline quantile regression with sklearn"):
 with Timer("Running B-spline quantile regression with sklearn"):
     bsq = {}
     for q in quantiles:
-        bs = SplineTransformer()
+        bs = SplineTransformer(n_knots=n_knots)
         bs.fit(x.reshape(-1, 1))
         tfx = bs.transform(xx.reshape(-1, 1))
         model = QuantileRegressor(quantile=q, alpha=0, solver="highs")
@@ -53,29 +56,28 @@ with Timer("Running linear spline quantile regression with statsmodels"):
     smlsq = {}
     for q in quantiles:
         smlsq[q] = LinearSpline.from_data(
-            xx, yy, knots=knots, q=q, method="QuantileRegression"
+            xx, yy, knots=np.linspace(0, 1, n_knots), q=q, method="QuantileRegression", max_iter=int(1e8)
         )
         print(".", end="")
 
 # Get cubic spline with statsmodels
 with Timer("Running natural cubic spline quantile regression with statsmodels"):
     smcsq = {}
-    # for q in [0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95]:
     for q in quantiles:
         smcsq[q] = NaturalCubicSpline.from_data(
-            xx, yy, knots=knots, q=q, method="QuantileRegression", max_iter=int(1e4)
+            xx, yy, knots=np.linspace(0, 1, n_knots), q=q, method="QuantileRegression", max_iter=int(1e8)
         )
         print(".", end="")
 
 # Plot everything
 with Timer("Plotting"):
     fig, ax = plt.subplots(1, 1)
-    ax.plot(x, spline(x), label="Signal and its quantiles", color="k")
+    ax.plot(x, y, label="Signal and its quantiles", color="k")
     print(".", end="")
     ax.scatter(xx, yy, alpha=0.1, label="Observed signal + noise")
     print(".", end="")
     for q in quantiles:
-        ax.plot(x, spline(x) + (x**2 + 0.1) * sps.norm.ppf(q), label=None, color="k")
+        ax.plot(x, y + noise(x) * sps.norm.ppf(q), label=None, color="k")
         print(".", end="")
     label = "Linear spline quantiles sklearn"
     for q, f in skllsq.items():
@@ -87,12 +89,12 @@ with Timer("Plotting"):
         ax.plot(x, val, label=label, linestyle=":", color="g")
         label = None
         print(".", end="")
-    label = "Linear spline quantiles statsmodels"
+    label = "Linear spline quantiles"
     for q, f in smlsq.items():
         ax.plot(x, f(x), label=label, linestyle="-.", color="m")
         label = None
         print(".", end="")
-    label = "Natural cubic spline quantiles statsmodels"
+    label = "Natural cubic spline quantiles"
     for q, f in smcsq.items():
         ax.plot(x, f(x), label=label, linestyle="-.", color="c")
         label = None
